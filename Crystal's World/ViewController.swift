96cb083d048b69963c7b2cd6d09c43c9fae9020e
Jasmine Hung <jasminehung@defaultteki-macbook-pro.local>
2018-11-29 09:09:37 +0800

    Initial Commit

---
+++ Crystal's World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Crystal's World
+//
+//  Created by Jasmine Hung on 2018/11/29.
+//  Copyright © 2018年 Jasmine Hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
